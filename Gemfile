9a0254f68f87666e301ea14508e9b3c95702650f
Miles Z. Sterrett <miles@mileszs.com>
2017-02-19 21:52:35 -0500

    Add some color

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "redd"
+gem "colorize"
 gem "pry"
--- checker.rb
+++ checker.rb
@@ -2,18 +2,18 @@ require "yaml"
 require "pp"
 require "pry"
 require "redd"
+require "colorize"
 
 config = YAML.load(File.read("./config.yml"))
+keebs = %w(Planck JD40 JD45 MiniVan Minorca Preonic MobiK Quark)
 
 r = Redd.it(:script, config["client_id"], config["secret"], config["username"], config["password"])
 r.authorize!
-
-keebs = %w(Planck JD40 JD45 MiniVan Minorca Preonic MobiK Quark)
 posts = r.subreddit_from_name("mechmarket").search("[US- AND (" + keebs.join(" OR ") + ")", sort: :new)
 
 puts "+#{'-'*170}+"
 posts.each do |post|
   date = Time.at(post[:created]).strftime("%d-%m-%y")
-  printf("| %8s | %-40s | %-114s |\n", date, post[:title][0,40], post[:url])
+  printf("| %8s | %-50s | %-128s |\n", date.light_green, post[:title][0,50].light_magenta, post[:url].blue)
   puts "-"*172
 end
